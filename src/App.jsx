bedd95667062307441f2444ea24eee5c0dbe2b3d
Debasish Debnath <ddebnath241@gmail.com>
2024-07-01 00:55:25 +0530

    change the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, useRoutes } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+// import LoginPage from "./pages/LoginPage";
 import Login from "./components/auth/login/Login";
 import Register from "./components/auth/register/Register";
 import { AuthProvider } from "./context/authContext";
 import Home from "./components/home";
-import Header from "./components/header";
+// import Header from "./components/header";
 // import Header from "./components/Header";
 
 function App() {
@@ -34,8 +34,8 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Header />
-        <div className="w-full h-screen flex flex-col">
+        {/* <Header /> */}
+        <div>
           <RoutesComponent />
         </div>
       </BrowserRouter>
